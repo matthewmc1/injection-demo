ef8990c5b5345fe9509142324b19f5086ad9fb91
Matthew McGibbon <matthewmcgibbon@Matthews-MacBook-Air.local>
2024-08-03 14:03:10 +0100

    circuit breaker

--- main.go
+++ main.go
@@ -2,37 +2,11 @@ package main
 
 import (
 	"context"
-	"errors"
 	"fmt"
-	"log/slog"
 	"validator/circuit/circuit"
 )
 
 var fnCount int
-
-func makeRequest(context.Context) (string, error) {
-	fmt.Println("hello")
-	return "request-service", errors.New("Request")
-}
-
-func Breaker(circuit func(context.Context) (string, error), failureThreshold uint) func(context.Context) (string, error) {
-	return func(ctx context.Context) (string, error) {
-		if failureThreshold == 0 {
-			fmt.Println("invalid threshold")
-			return "Error with threshold value", errors.New("invalid threshold value")
-		}
-
-		fnCount++
-		slog.Info("function invocation", "count", fnCount)
-		if fnCount < int(failureThreshold) {
-			fmt.Println("under request threshold, request is allowed")
-			return "request ok", nil
-		} else {
-			fmt.Println("equal to or over request threshold")
-			return "request blocked", errors.New("threshold exceeded")
-		}
-	}
-}
 
 func main() {
 	c := context.Background()
